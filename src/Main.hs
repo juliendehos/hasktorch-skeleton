02d34d74b9c5d1a0bf78cc641b96274597150f7c
Julien Dehos <dehos@univ-littoral.fr>
2021-01-14 18:03:20 +0100

    test from two-layer-sinc

---
+++ src/Main.hs
@@ -0,0 +1,75 @@
+-- https://github.com/tscholak/two-layer-network
+
+import SincData
+
+import Control.Monad.Trans.State (evalStateT)
+import GHC.Float (float2Double)
+import Graphics.Vega.VegaLite
+import Prelude hiding (filter)
+import System.Random (getStdGen)
+
+main :: IO ()
+main = do
+    putStrLn "this is sinc"
+    (trainData, evalData) <- 
+        getStdGen >>= evalStateT (
+            (,) <$> mkSincData "train" 10
+                <*> mkSincData "eval" 5)
+    print trainData
+    print evalData
+
+{-
+    let mkDataRow x y = dataRow
+          [ ("x", Number . float2Double $ x),
+            ("y", Number . float2Double $ y)
+          ] 
+    let trainD = dataFromRows [] . foldr (uncurry mkDataRow) [] . _data $ trainData
+-}
+
+    let cars =  dataFromUrl "https://vega.github.io/vega-datasets/data/cars.json" []
+        enc = encoding
+                . position X [ PName "Horsepower", PmType Quantitative ]
+                . position Y [ PName "Miles_per_Gallon", PmType Quantitative, PTitle "Miles per Gallon" ]
+                . color [ MName "Origin" ]
+        bkg = background "rgba(0, 0, 0, 0.05)"
+    toHtmlFile "out.html" $ toVegaLite [ bkg, cars, mark Circle [MTooltip TTEncoding], enc [] ]
+
+
+
+mkVegaLite :: Data -> VegaLite
+mkVegaLite dataset =
+  let -- width and height of the individual plots (in pixels)
+      w = width 700
+      h = height 350
+
+      encOverview =
+        encoding
+          . position X [PName "x", PmType Quantitative]
+          . position Y [PName "y", PmType Quantitative, PScale scaleOptsOverview]
+      scaleOptsOverview = [SDomain (DNumbers [-2, 2]), SNice (IsNice False)]
+
+      transOverview =
+        transform
+          . filter (FRange "x" (NumberRange (-20) 20))
+          . filter (FRange "y" (NumberRange (-2) 2))
+
+      evaluation =
+        asSpec
+          [ dataFromSource "evaluation" [],
+            transOverview [],
+            mark Point [MSize 5, MStroke "black"]
+          ]
+
+      overview =
+        asSpec
+          [ layer [evaluation],
+            encOverview [],
+            w,
+            h
+          ]
+
+   in toVegaLite
+        [ dataset,
+          vConcat [overview]
+        ]
+
---
+++ src/SincData.hs
@@ -0,0 +1,27 @@
+module SincData where
+
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State (StateT (..), state)
+import Data.Random.Normal (normal)
+import Data.Text (Text)
+import System.Random (Random, RandomGen)
+
+sinc :: Floating a => a -> a
+sinc a = sin a / a
+
+noisySinc :: (Floating a, Random a, RandomGen g) => a -> a -> g -> (a, g)
+noisySinc eps a g = let (noise, g') = normal g in (sinc a + eps * noise, g')
+
+data SincData = SincData 
+    { _name :: Text
+    , _data :: [(Float, Float)]
+    } deriving (Eq, Ord, Show)
+
+mkSincData :: (RandomGen g, Monad m) => Text -> Int -> StateT g m SincData
+mkSincData name size =
+  let next = do
+        x <- (* 20) <$> state normal
+        y <- state (noisySinc 0.05 x)
+        pure (x, y)
+   in SincData name <$> replicateM size next
+
